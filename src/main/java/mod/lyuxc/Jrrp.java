8ce4b2826c54c307018eab7e9b472740d1a7fe80
lyuxc-unknow <2675796669@qq.com>
2024-01-23 18:50:14 +0800

    code clean

--- src/main/java/mod/lyuxc/Jrrp.java
+++ src/main/java/mod/lyuxc/Jrrp.java
@@ -1,13 +1,44 @@
 package mod.lyuxc;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
+import java.util.Calendar;
+import java.util.Objects;
+import java.util.Random;
+
 @Mod(modid = "jrrp",name = "Jrrp",version = "1.0.0")
+@Mod.EventBusSubscriber
 public class Jrrp{
+    private static final Calendar CALENDAR = Calendar.getInstance();
+    private static final String DAY_OF_YEAR = String.valueOf(CALENDAR.get(Calendar.DAY_OF_YEAR));
+    private static final Random RANDOM_SOURCE = new Random();
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new JrrpCommand());
+    }
+    public static void JrrpEvent(Entity entity) {
+        NBTTagCompound compoundTag = entity.getEntityData();
+        if (compoundTag.getString("jrrpLaseTime")==null||compoundTag.getString("jrrp") == null || !Objects.requireNonNull(compoundTag.getString("jrrpLaseTime")).equals(DAY_OF_YEAR)) {
+            entity.getEntityData().setString("jrrpLaseTime", DAY_OF_YEAR);
+            entity.getEntityData().setString("jrrp", String.valueOf(RANDOM_SOURCE.nextInt(101)));
+            compoundTag.setString("jrrpLaseTime", DAY_OF_YEAR);
+            compoundTag.setString("jrrp", String.valueOf(RANDOM_SOURCE.nextInt(101)));
+        }
+        int jrrpValue = Integer.parseInt(Objects.requireNonNull(compoundTag.getString("jrrp")));
+        String tipsText = JrrpConfig.tipsText;
+        if (jrrpValue < 50) {
+            tipsText = tipsText.replace("$w",JrrpConfig.PROMPT_WORD_LOW[RANDOM_SOURCE.nextInt(JrrpConfig.PROMPT_WORD_LOW.length)]);
+        } else if(jrrpValue < 90) {
+            tipsText = tipsText.replace("$w",JrrpConfig.PROMPT_WORD_HIGH[RANDOM_SOURCE.nextInt(JrrpConfig.PROMPT_WORD_HIGH.length)]);
+        } else {
+            tipsText = tipsText.replace("$w",JrrpConfig.PROMPT_WORD_HIGHEST[RANDOM_SOURCE.nextInt(JrrpConfig.PROMPT_WORD_HIGHEST.length)]);
+        }
+        tipsText = tipsText.replace("$v",String.valueOf(jrrpValue)).replace("\\n","\n");
+        entity.sendMessage(new TextComponentString(tipsText));
     }
 }
--- src/main/java/mod/lyuxc/JrrpCommand.java
+++ src/main/java/mod/lyuxc/JrrpCommand.java
@@ -1,21 +1,13 @@
 package mod.lyuxc;
 
 import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.TextComponentString;
 
-import java.util.Calendar;
 import java.util.Objects;
-import java.util.Random;
 
 public class JrrpCommand extends CommandBase implements ICommand {
-    private static final Calendar CALENDAR = Calendar.getInstance();
-    private static final String DAY_OF_YEAR = String.valueOf(CALENDAR.get(Calendar.DAY_OF_YEAR));
-    private static final Random RANDOM_SOURCE = new Random();
     @Override
     public String getName() {
         return "jrrp";
@@ -27,25 +19,8 @@ public class JrrpCommand extends CommandBase implements ICommand {
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        NBTTagCompound compoundTag = Objects.requireNonNull(sender.getCommandSenderEntity()).getEntityData();
-        if (compoundTag.getString("jrrpLaseTime")==null||compoundTag.getString("jrrp") == null || !Objects.requireNonNull(compoundTag.getString("jrrpLaseTime")).equals(DAY_OF_YEAR)) {
-            sender.getCommandSenderEntity().getEntityData().setString("jrrpLaseTime", DAY_OF_YEAR);
-            sender.getCommandSenderEntity().getEntityData().setString("jrrp", String.valueOf(RANDOM_SOURCE.nextInt(101)));
-            compoundTag.setString("jrrpLaseTime", DAY_OF_YEAR);
-            compoundTag.setString("jrrp", String.valueOf(RANDOM_SOURCE.nextInt(101)));
-        }
-        int jrrpValue = Integer.parseInt(Objects.requireNonNull(compoundTag.getString("jrrp")));
-        String tipsText = JrrpConfig.tipsText;
-        if (jrrpValue < 50) {
-            tipsText = tipsText.replace("$w",JrrpConfig.PROMPT_WORD_LOW[RANDOM_SOURCE.nextInt(JrrpConfig.PROMPT_WORD_LOW.length)]);
-        } else if(jrrpValue < 90) {
-            tipsText = tipsText.replace("$w",JrrpConfig.PROMPT_WORD_HIGH[RANDOM_SOURCE.nextInt(JrrpConfig.PROMPT_WORD_HIGH.length)]);
-        } else {
-            tipsText = tipsText.replace("$w",JrrpConfig.PROMPT_WORD_HIGHEST[RANDOM_SOURCE.nextInt(JrrpConfig.PROMPT_WORD_HIGHEST.length)]);
-        }
-        tipsText = tipsText.replace("$v",String.valueOf(jrrpValue)).replace("\\n","\n");
-        sender.sendMessage(new TextComponentString(tipsText));
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args)  {
+        Jrrp.JrrpEvent(Objects.requireNonNull(sender.getCommandSenderEntity()));
     }
 
     @Override
